f7795c0f32875d0ab2be24d028ac73993210a26a
Ibselibs <92301976+Ibselibs@users.noreply.github.com>
2023-07-27 11:11:32 +0200

    Create button in HTML

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // // document.getElementById("count").innerText = 5
 
- let count = 5
+// let count = 5
+// count = 3
+// console.log(count)
 
-count = 3
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+let bonusPoints = 50
+console.log(bonusPoints)
 
-count = count + 10
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
 
-count = count - 2
+// Decrease it down to 25, and then finally increase it to 70
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
 
-console.log(count)
+bonusPoints = bonusPoints + 45
 
+// Console.log the value after each step
+console.log(bonusPoints)
