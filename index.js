099803249dd2ddd7a79d49f69be73e8c62c3b489
Ibselibs <92301976+Ibselibs@users.noreply.github.com>
2023-07-27 11:04:18 +0200

    leger med tal

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 // // document.getElementById("count").innerText = 5
 
-// let firstBatch = 5
-// let secondBatch = 7
+ let count = 5
 
-// let count = firstBatch
+count = 3
 
-// console.log(count)
+count = count + 10
 
-// 1. Create two variables, myAge and humanDogRatio
-let myAge = 25
-let humanDogRatio = 7
+count = count - 2
 
-// 2. Miltuply the two together and store the result in myDogAge
-let myDogAge = myAge * humanDogRatio
+console.log(count)
 
-// 3. Log myDogAge to the console.
-console.log(myDogAge)
