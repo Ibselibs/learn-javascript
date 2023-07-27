11bc76c3e4f2b27eafffff74a64aab25721172ec
Ibselibs <92301976+Ibselibs@users.noreply.github.com>
2023-07-27 10:44:43 +0200

    Ida Test

---
+++ index.js
@@ -0,0 +1,18 @@
+// // document.getElementById("count").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch
+
+// console.log(count)
+
+// 1. Create two variables, myAge and humanDogRatio
+let myAge = 25
+let humanDogRatio = 7
+
+// 2. Miltuply the two together and store the result in myDogAge
+let myDogAge = myAge * humanDogRatio
+
+// 3. Log myDogAge to the console.
+console.log(myDogAge)
