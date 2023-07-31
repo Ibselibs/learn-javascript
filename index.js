aab478c54ab7b4ab8f00b3b113d68a6598ffeb63
Ibselibs <92301976+Ibselibs@users.noreply.github.com>
2023-07-31 11:16:43 +0200

    Finalized people entering app

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// Create two variables, name and greeting. The name variable should store your name,
-// and the greeting should store e.g. "Hi, my name is "
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
-let name = "Ida"
-let greeting = "Hi, my name is "
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
 
-// Create a third variable, myGreeting, that contatenates the two strings
-// Log myGreeting to the console
-let myGreeting = greeting + name
-
-console.log(myGreeting)
-
-9
-24
-51
-100100
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
